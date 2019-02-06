b8f5627c9be6f506d78d5840054539063bb035dc
Pranacahya <pranacahya_13@yahoo.com>
2019-02-06 20:59:15 +0700

    ModulMenggambar #2

---
+++ Evaluasi Psikomotorik Bangun Ruang dan Datar/Assets/Source Code/KeyboardEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKey(vKey))
+            {
+                Debug.Log(vKey);
+
+            }
+        }
+
+    }
+}
